5be013dbcb83fa238aea44de787bd805f1db5ed5
PetrS12 <asprado@rambler.ru>
2020-11-04 22:39:40 +0300

    changed local host in .sh for dev

--- deploy/app/start-django-dev.sh
+++ deploy/app/start-django-dev.sh
@@ -2,4 +2,4 @@
 
 wait-for-it "${PARROT_DB_HOST:-parrot-database}":5432 -s -t 180 \
 && python /app/src/manage.py migrate --noinput \
-&& python /app/src/manage.py runserver 0.0.0.0:8042
+&& python /app/src/manage.py runserver 127.0.0.1:8042
