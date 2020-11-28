4a9c16971821e65f94f2b556c5071fdac011af88
PetrS12 <asprado@rambler.ru>
2020-11-28 13:54:43 +0300

    refactored headers_to_list: added json.loads

--- http_stubs/templatetags/stub_tags.py
+++ http_stubs/templatetags/stub_tags.py
@@ -1,4 +1,6 @@
+import json
 from html import unescape
+from json import JSONDecodeError
 from typing import AnyStr, Dict, List
 
 from django import template
@@ -20,13 +22,21 @@ def get_absolute_url_tag(context: Dict, url: Url) -> Url:
     return context.get('request').build_absolute_uri(url)
 
 
-@register.filter(name='headers_to_list')
+@register.filter()
 @stringfilter
-def get_headers_list(headers: AnyStr, separator: AnyStr) -> List:
+def headers_to_list(headers: AnyStr) -> List:
     """Filter that creates a list from a string of headers(dict).
 
-    :param headers: string of headers
-    :param separator: separator to separate string
-    :returns: list of headers
+    Return a list of the lines in the string representation of the request
+    headers.
+    :param headers: string representation of the request headers
+    :returns: list of individual headers string representation
     """
-    return unescape(headers).lstrip('{').rstrip('}').split(separator)
+    try:
+        headers = json.loads(unescape(headers).replace('\'', '\"'))
+    except (TypeError, JSONDecodeError):
+        return []
+    return [
+        f'{header}: {header_value}'
+        for header, header_value in headers.items()
+    ]
