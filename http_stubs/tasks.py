64044d7e06bc71289f2b29aca8fe8f14905ade4d
lowit <ea@lowit.ru>
2020-12-20 22:35:04 +0300

    issue-38: adds restricted python to task

--- http_stubs/tasks.py
+++ http_stubs/tasks.py
@@ -1,3 +1,12 @@
+import json
+
+import requests
+from RestrictedPython import (
+    compile_restricted,
+    limited_builtins,
+    safe_builtins,
+    utility_builtins,
+)
 from billiard.exceptions import SoftTimeLimitExceeded
 
 from http_stubs.models import LogEntry
@@ -8,7 +17,17 @@ from parrot import celery_app
 def run_request_script(log_id: int, script: str) -> None:
     log: LogEntry = LogEntry.objects.get(pk=log_id)
     try:
-        print(f'Request: {script!r}')
+        byte_code = compile_restricted(script)
+        builtins = {
+            **safe_builtins,
+            **limited_builtins,
+            **utility_builtins,
+        }
+        loc = {
+            'requests': requests,
+            'json': json,
+        }
+        exec(byte_code, {'__builtins__': builtins}, loc)
     except SoftTimeLimitExceeded:
         log.result_script = 'Time limit'
     except Exception as err:
